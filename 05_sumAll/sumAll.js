1463095b777e5a815c1614258e845bed0ecb29ca
HampusG1 <hampusgranqvist3@gmail.com>
2024-08-26 14:36:44 +0200

    Passed all the tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,12 @@
-const sumAll = function() {
+const sumAll = function (a, b) {
+  if (!Number.isInteger(a) || !Number.isInteger(b)) return "ERROR";
+  if (a < 0 || b < 0) return "ERROR";
+  let lower = Math.min(a, b);
+  let upper = Math.max(a, b);
 
+  let n = upper - lower + 1;
+  let totalSum = (n * (lower + upper)) / 2;
+  return totalSum;
 };
 
 // Do not edit below this line
